27ccfa83dc16cbb196930a71bbabcbba899f5436
meharaj-git <m_meharaj@yahoo.co.in>
2022-02-28 00:28:45 +0800

    feature python programmes_v1

---
+++ app.py
@@ -0,0 +1,9 @@
+is_new = True
+name = input('What is your name? ')
+age = 20
+
+print ('Patient Name : ', name)
+print ('Age : ', age)
+print ('is new patient : ', is_new)
+faviourite_color = input('what is your favourite color? ')
+print (name + ' likes ' + faviourite_color)
